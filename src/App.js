e62dca665cca6610bfe314600431e7b530e1f83a
Nathan Ferraz <nathanferraz@hotmail.com>
2022-01-06 12:32:14 -0300

    criando o componente RegistrationForm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {Container, Typography } from "@material-ui/core"
-import {validarCPF, validarSenha} from "./models/cadastro"
+import { Container, Typography } from "@material-ui/core"
+import { validarCPF, validarSenha } from "./models/cadastro"
 import ValidacoesCadastro from './contexts/ValidacoesCadastro'
 import "./App.css";
 import 'fontsource-roboto';
@@ -8,18 +8,18 @@ import FormularioCadastro from "./components/Formulario";
 
 function App() {
 
-function aoEnviarForm(dados){
-console.log(dados);
-}
+  function aoEnviarForm(dados) {
+    console.log(dados);
+  }
 
-return (
-<Container component="article" maxWidth="sm">
-<Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography>
-<ValidacoesCadastro.Provider value={{ cpf: validateCPF, senha: validatePassword }}>
-<FormularioCadastro aoEnviar={aoEnviarForm} validacoes={{cpf:validarCPF, senha:validarSenha, nome:validarSenha}} />
-</ValidacoesCadastro.Provider>
-</Container>
-)
+  return (
+    <Container component="article" maxWidth="sm">
+      <Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography>
+      <ValidacoesCadastro.Provider value={{ cpf: validateCPF, senha: validatePassword }}>
+        <FormularioCadastro aoEnviar={aoEnviarForm} validacoes={{ cpf: validarCPF, senha: validarSenha, nome: validarSenha }} />
+      </ValidacoesCadastro.Provider>
+    </Container>
+  )
 }
 
 export default App
